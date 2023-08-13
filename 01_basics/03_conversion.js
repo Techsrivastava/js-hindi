ca45c95c7f82c90277669f947dbd140a7446e2ee
Techsrivastava <Adarsh00761@gmail.com>
2023-08-13 16:59:06 +0530

    updated code

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -28,5 +28,42 @@ let booleanIsloggedIn = Boolean(isloggedIn)
 let someNumber = 33
  
 let strigNumber =  String(someNumber)
-console.log(strigNumber);
-console.log( typeof strigNumber);
+// console.log(strigNumber);
+// console.log( typeof strigNumber);
+
+
+
+// ***************************** Opration ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+let str1 = "Hello"
+let str2= ' World'
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+
+num1 =num2 =num3 = 2 + 2 
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
