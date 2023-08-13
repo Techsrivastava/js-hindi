deaf8e68d6329ee56df5aeeaf8de9ad12ccf2163
Techsrivastava <Adarsh00761@gmail.com>
2023-08-13 13:22:11 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,32 @@
+// let score =" 33abc"
+// let score = null
+// let score = undefined
+// let score = undefined
+// let score = true
+// let score = "Adarsh"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isloggedIn = "Adarsh"
+
+let booleanIsloggedIn = Boolean(isloggedIn)
+// console.log(booleanIsloggedIn);
+
+
+let someNumber = 33
+ 
+let strigNumber =  String(someNumber)
+console.log(strigNumber);
+console.log( typeof strigNumber);
